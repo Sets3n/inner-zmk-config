6dceae6f102a71ae3c1a394bf761bc16c06a253b
Sets3n <li.xuebin@outlook.com>
2022-01-13 11:30:34 +0800

    fix

--- config/boards/arm/inner/board.cmake
+++ config/boards/arm/inner/board.cmake
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MIT
 # iNNER (with eByte E73-2G4M08S1C nrf52840 System-on-module) configuration
 
-#set(OPENOCD_NRF5_SUBFAMILY nrf52)
+set(OPENOCD_NRF5_SUBFAMILY nrf52)
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
